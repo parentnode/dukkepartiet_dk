6ded0b8ad3a8da397d106e559215fd29ff29bb9e
kaestel <kaestel@e-types.com>
2014-03-20 15:50:21 +0100

    updated slogans

--- src/templates/pages/404.php
+++ src/templates/pages/404.php
@@ -1,8 +1,7 @@
 <div class="scene i:scene 404">
 	<h1>404</h1>
-	<h2>Page missing</h2>
+	<h2>Siden mangler</h2>
 	<p>
-		We wish we had a good explanation, but something happened we weren't prepared for. 
-		Please taunt our lead developer, by sending an email to <a href="mailto:kaestel@e-types.com">kaestel@e-types.com</a>.
+		Vi beklager, men der er sket en fejl.
 	</p>
 </div>
