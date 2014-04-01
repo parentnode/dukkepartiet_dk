16c52ee690357e0ca0c4e6e309eb4b51c916b6ae
robinisaksson <robin.isaksson@gmail.com>
2014-04-01 13:04:42 +0200

    includes

--- src/www/js/lib/seg_desktop_light_include.js
+++ src/www/js/lib/seg_desktop_light_include.js
@@ -1,5 +1,21 @@
 document.write('<script type="text/javascript" src="/js/manipulator/v0_7_5/full/seg_desktop_light.js"></script>');
 
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-image.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-preloader.js"></script>');
+
+
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-page-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-front-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-carousel-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-events-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-event-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-about-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-action-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-candidates-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-candidate-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-support-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-help-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop/i-footer-desktop.js"></script>');
 
 // document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 // document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
