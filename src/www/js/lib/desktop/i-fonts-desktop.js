0305eabe1f7da66b9c0b7bc4996e57a78926ec93
robinisaksson <robin.isaksson@gmail.com>
2014-04-02 12:20:44 +0200

    fonts update

--- src/www/js/lib/desktop/i-fonts-desktop.js
+++ src/www/js/lib/desktop/i-fonts-desktop.js
@@ -1,1 +1,2 @@
-document.write('<script type="text/javascript" src="http://fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>');
+var fonts_com_url = '<script type="text/javascript" src="http://fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>';
+document.write(fonts_com_url);
