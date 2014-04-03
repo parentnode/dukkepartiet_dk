d97c5f67726259f03db89fd80b563a768bd6ca6e
robinisaksson <robin.isaksson@gmail.com>
2014-04-03 13:42:21 +0200

    update help_us - build

--- src/templates/pages/help_us.php
+++ src/templates/pages/help_us.php
@@ -11,15 +11,28 @@
 
 		<p class="intro">Dukkepartiet vil gerne i Folketinget. Derfor har vi brug for din underskrift. For at blive opstillingsberettiget til det kommende Folketingsvalg skal Dukkepartiet samle 20.260 underskrevne vælgererklæringer. Ved at underskrive vælgererklæringen bliver du stiller for Dukkepartiet, men du er ikke forpligtet på at stemme på Dukkepartiet ved Folketingsvalget.</p>
  
-		<p><a href="/img/vaelgererklaering_dukkepartiet.pdf" download>Her kan du hente vælgererklæringen.</a></p>
+		<p class="download"><a href="/img/vaelgererklaering_dukkepartiet.pdf" download>Her kan du hente vælgererklæringen.</a></p>
 		<h3>Vejledning:</h3>
-		<ul>
+		<dl>
+			<dt>1</dt>
+			<dd>Udskriv vælgererklæringen.</dd>
+			<dt>2</dt>
+			<dd>Udfyld og underskriv. Husk at datere og underskrive vælgererklæringen ellers er den ugyldig.</dd>
+			<dt>3</dt>
+			<dd>Send vælgererklæringen til Dukkepartiet, Postbox 2030, 1112 København K.</dd>
+			<dt>4</dt>
+			<dd>Dukkepartiet sender din vælgererklæring videre til Folkeregisteret i din kommune.</dd>
+			<dt>5</dt>
+			<dd>Folkeregisteret godkender og sender derefter vælgererklæringen til dig, hvorefter du igen skal sende den til Dukkepartiet.</dd>
+		</dl>
+
+		<!--ul>
 			<li><p>- Udskriv vælgererklæringen.</p></li>
 			<li><p>- Udfyld og underskriv. Husk at datere og underskrive vælgererklæringen ellers er den ugyldig.</p></li>
 			<li><p>- Send vælgererklæringen til Dukkepartiet, Postbox 2030, 1112 København K.</p></li>
 			<li><p>- Dukkepartiet sender din vælgererklæring videre til Folkeregisteret i din kommune.</p></li>
 			<li><p>- Folkeregisteret godkender og sender derefter vælgererklæringen til dig, hvorefter du igen skal sende den til Dukkepartiet.</p></li>
-		</ul>
+		</ul-->
 		
 		<p>Det er en forudsætning for at blive stiller, at du er dansk statsborger, er bosat i Danmark og har ret til at stemme ved Folketingsvalget.</p>
  
