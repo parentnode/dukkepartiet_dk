fbece45274c28c572263d39bcd961031ab9b3391
robinisaksson <robin.isaksson@gmail.com>
2014-05-07 15:43:00 +0200

    update font loading (https)

--- src/www/js/lib/desktop/i-fonts-desktop.js
+++ src/www/js/lib/desktop/i-fonts-desktop.js
@@ -1,2 +1,2 @@
-var fonts_com_url = '<script type="text/javascript" src="http://fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>';
+var fonts_com_url = '<script type="text/javascript" src="//fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>';
 document.write(fonts_com_url);
