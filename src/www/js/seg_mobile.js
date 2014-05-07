68db6cead5ceccd303bed1f390111bd037bb9d16
robinisaksson <robin.isaksson@gmail.com>
2014-05-07 16:08:34 +0200

    fonts.com httpS

--- src/www/js/seg_mobile.js
+++ src/www/js/seg_mobile.js
@@ -4749,7 +4749,7 @@ Util.Objects["carousel"] = new function() {
 }
 
 /*i-fonts-desktop.js*/
-var fonts_com_url = '<script type="text/javascript" src="http://fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>';
+var fonts_com_url = '<script type="text/javascript" src="https://fast.fonts.net/jsapi/013abfcd-c15e-468e-aebc-4b44edca5e9f.js"></script>';
 document.write(fonts_com_url);
 
 
