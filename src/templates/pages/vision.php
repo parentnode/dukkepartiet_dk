298f1ec85a9784a3a9f8fb1518a60cd1f6216d2e
kaestel <kaestel@e-types.com>
2014-06-08 19:53:11 +0200

    Vision launch

--- src/templates/pages/vision.php
+++ src/templates/pages/vision.php
@@ -21,24 +21,4 @@ $items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "order" => 
 <?	endif; ?>
 	</div>
 
-	<div class="container">
-		<ul>
-			<li>Dukkepartiet er en mistillidserklæring til politikerne.</li>
-			<li>Dukkepartiet er den nødvendige desperate latterliggørelse af politikerne.</li>
-			<li>Dukkepartiet mener, at politikerne har mistet realitetssansen og kontakten til borgerne.</li>
-			<li>Dukkepartiet symboliserer og konkretiserer en længsel efter en ændring.</li>
-			<li>Dukkepartiet vil gøre op med politikere, der lyver. Politikere, der ikke vil andet end at angribe hinanden. Politikere, der taler et tomt spin-sprog. Politikere, der er karrierefikserede, personfikserede og uden visioner.</li>
-			<li>Dukkepartiet vil tvinge<br> politikerne til at ændre adfærd.</li>
-			<li>Dukkepartiet vil udstille politikernes sprog, deres karrierejagt og deres manglende visioner.</li>
-			<li>Dukkepartiet vil slå hul på det politiske system og muliggøre, at visioner igen kan få plads.</li>
-			<li>Dukkepartiet vil tvinge politikerne til at tage ansvar for den latterliggørelse af demokratiet, borgerne igen og igen udsættes for.</li>
-			<li>Dukkepartiet vil præsentere en række lovforslag, der skal give politikerne realitetssansen tilbage.</li>
-			<li>Dukkepartiet stiller op til Folketinget. Bliver Dukkepartiet valgt ind, vil vi støtte den koalition, der lader Dukkepartiet varetage Statsministerposten i 5 uger. I de 5 uger vil Dukkepartiet indføre en kollektiv tænkepause.</li>
-			<li>Dukkepartiet vil udvælge en almindelig borger og lade denne borger være partiets Statsministerkandidat.</li>
-			<li>Dukkepartiet giver de danske borgere mulighed for at sende et klart signal til Folketingets politikere.</li>
-			<li>Dukkepartiet stiller spørgsmålet: Kan befolkningen fortsat sætte deres kryds ved de politikere, de så åbenlyst har mistillid til?</li>
-			<li>Følg med på <a href="http://www.facebook.com/dukkepartiet" target="_blank">Facebook</a> og her på hjemmesiden.</li>
-		</ul>		
-	</div>
-
 </div>
