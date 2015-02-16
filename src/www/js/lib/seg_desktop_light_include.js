0446262c08096f69c6cf7920fb0374eb54124af4
kaestel <kaestel@e-types.com>
2015-02-16 14:30:50 +0000

    css fix

--- src/www/js/lib/seg_desktop_light_include.js
+++ src/www/js/lib/seg_desktop_light_include.js
@@ -3,7 +3,7 @@ document.write('<script type="text/javascript" src="/js/manipulator/v0_9/full/se
 document.write('<script type="text/javascript" src="/js/manipulator/v0_9/src/u-textscaler.js"></script>');
 
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-page-desktop.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/desktop_light/i-page-desktop_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-footer-desktop.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-fonts-desktop.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/desktop/i-scene-desktop.js"></script>');
@@ -28,8 +28,8 @@ document.write('<script type="text/javascript" src="/js/lib/desktop/i-vision-des
 //document.write('<script type="text/javascript" src="/js/lib/desktop/i-candidate-desktop.js"></script>');
 
 
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-declaration-desktop.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/u-form-custom.js"></script>');
+// document.write('<script type="text/javascript" src="/js/lib/desktop/i-declaration-desktop.js"></script>');
+// document.write('<script type="text/javascript" src="/js/lib/desktop/u-form-custom.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
