5769a3490f56ced6fe953c9b378d3660d1c0d8ac
kaestel <martin@think.dk>
2015-03-31 21:05:43 +0200

    new frontage

---
+++ src/templates/pages/index.php
@@ -0,0 +1,21 @@
+<?php
+global $action;
+
+$IC = new Items();
+$itemtype = "counter";
+
+$items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "extend" => true, "limit" => 1));
+if($items) {
+	$item = $items[0];
+
+}
+?>
+<div class="scene front i:front red">
+
+<?	if($item): ?>
+	<h1><?= $item["text"] ?></h1>
+	<h2><span><?= implode("</span><span>", str_split($item["counter"])) ?></span></h2>
+	<p>Vælgererklæringer</p>
+<?	endif; ?>
+
+</div>
